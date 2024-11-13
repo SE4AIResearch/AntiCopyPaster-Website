7adf3b7b9409fc632080a1ee6b847507729ad4f5
acceleratorxm7 <acceleratorxm7@gmail.com>
2024-11-13 10:02:25 -0500

    add github action & update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/AntiCopyPaster-Website",
   build: {
-    outDir: "./build",
     rollupOptions: {
       output: {
         manualChunks(id) {
